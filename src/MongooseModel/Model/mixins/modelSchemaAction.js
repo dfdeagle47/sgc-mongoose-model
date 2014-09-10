0a3bde5fa4af09ca17262b92416b6cc154d4a7a4
Yvan de Ghellinck <yvan@ghellinck.be>
2014-09-10 17:38:35 +0200

    .gitignore is now working

--- src/MongooseModel/Model/mixins/modelSchemaAction.js
+++ src/MongooseModel/Model/mixins/modelSchemaAction.js
@@ -1,38 +1,39 @@
 define([
-	'saga/ajax/SGAjax'
-], function (SGAjax) {
+	// 'saga/ajax/SGAjax'
+], function (/*SGAjax*/) {
 	'use strict';
 
 	return function(/*SagaModel*/){
 		return {
 			'do': function(action, args){
-				var actionUrl = this._generateUrl() + '/' + action;
+				// revoir
+				// var actionUrl = this._generateUrl() + '/' + action;
 				
-				if(args instanceof Array) {
-					var argsObj = {};
-					if(this.schema.actions[action]){
-						if (this.schema.actions[action].args) {
-							this.schema.actions[action].args.forEach(function(arg, i){
-								argsObj[arg] = args[i];
-							});
-						}
-					}
-					args = argsObj;
-				}
+				// if(args instanceof Array) {
+				// 	var argsObj = {};
+				// 	if(this.schema.actions[action]){
+				// 		if (this.schema.actions[action].args) {
+				// 			this.schema.actions[action].args.forEach(function(arg, i){
+				// 				argsObj[arg] = args[i];
+				// 			});
+				// 		}
+				// 	}
+				// 	args = argsObj;
+				// }
 
-				var deferred = SGAjax.ajax({
-					type: 'POST',
-					url: actionUrl,
-					data: args || {}
-				});
+				// var deferred = SGAjax.ajax({
+				// 	type: 'POST',
+				// 	url: actionUrl,
+				// 	data: args || {}
+				// });
 
-				var me = this;
-				deferred.done(function(){
-					me.trigger('action', args);
-					me.trigger('action:'+action, args);
-				});
+				// var me = this;
+				// deferred.done(function(){
+				// 	me.trigger('action', args);
+				// 	me.trigger('action:'+action, args);
+				// });
 
-				return deferred;
+				// return deferred;
 			}
 		};
 	};

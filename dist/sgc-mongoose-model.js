1b7594251f9ddb87aa829f82e470d963a8c8d3e6
Yvan de Ghellinck <yvan@ghellinck.be>
2014-12-04 14:21:58 +0100

    bug fix

--- dist/sgc-mongoose-model.js
+++ dist/sgc-mongoose-model.js
@@ -918,12 +918,9 @@ define('RemoteModel/mixins/validation',[], function () {
 				if (descriptor.isValidable()) {
 					options = _.clone(options);
 					var attriVal = this.get(attribute, {lazyCreation:options.lazyCreation});
-					if (!attriVal.validate) {
-						return;
-					};
 					var subOptions = _.clone(options);
 					subOptions.lazyCreation = false;
-					var error = (attriVal && (attriVal.validate(undefined, subOptions)));
+					var error = (attriVal && attriVal.validate && (attriVal.validate(undefined, subOptions)));
 					if (error) {
 						return error;
 					}
